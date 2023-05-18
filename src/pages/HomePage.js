12ac21263d6add8ba706a6cf00763ddc05b8cad1
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-05-18 20:19:57 +0300

    assaa

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Utils/Header'
+import MidBar from '../components/Utils/MidBar'
+import DownBar from '../components/Utils/DownBar'
+
+const HomePage = () => {
+  return (
+    <div className='HomePage'>
+          <Header/>
+          <MidBar/>
+          <DownBar/>
+    </div>
+  )
+}
+
+export default HomePage
