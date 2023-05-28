8ed0723245690c78aa1a2e792e7cde42352695af
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-05-28 14:54:30 +0300

    asd

--- src/pages/Cart/CartForm.js
+++ src/pages/Cart/CartForm.js
@@ -15,21 +15,7 @@ const CartForm = () => {
       </div>
       <div className="col text-right">€ 132.00</div>
     </div>
-    <form>
-      <p>SHIPPING</p>
-      <select>
-        <option className="text-muted">Standard-Delivery- €5.00</option>
-      </select>
-      <p>GIVE CODE</p>
-      <input id="code" placeholder="Enter your code" />
-    </form>
-    <div
-      className="row"
-      style={{ borderTop: "1px solid rgba(0,0,0,.1)", padding: "2vh 0" }}
-    >
-      <div className="col">TOTAL PRICE</div>
-      <div className="col text-right">€ 137.00</div>
-    </div>
+     
     <button className="btn">CHECKOUT</button>
   </div>
   )
